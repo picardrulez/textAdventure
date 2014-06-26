898be265127b153beb4471b5caef323059dae589
Geoff <picardrulez@gmail.com>
2014-06-26 17:02:21 -0400

    updated debugger

--- equippingDebugTest/main.cpp
+++ equippingDebugTest/main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <string>
 
@@ -9,11 +7,6 @@ int player_inv[25][3] = {
     {0,0,50},
     {0,1,75},
     {0,2,100},
-    {0,3,300},
-    {0,4,75},
-    {0,5,100},
-    {0,6,250},
-    {0,7,500}
 };
 
 int player_eweapon[1][3] = {69,69,69};
@@ -32,7 +25,7 @@ int playerinput;
 void equipItem(int table, int itemNumber, int uses);
 void dropItem(int itemNumber);
 void equipWeapon(int table, int itemNumber, int uses);
-int playerItemCount = 8;
+int playerItemCount = 3;
 int itemUses;
 int etable = 0;
 int eitemNumber = 0;
@@ -107,6 +100,8 @@ int main()
 
 int equipMenu()
 {
+    cout << "starting equipMenu function\n";
+    cout << "playerItemCount is " << playerItemCount << "\n";
     int doneEquiping = 0;
     while (doneEquiping == 0)
     {
@@ -128,7 +123,9 @@ int equipMenu()
                 }
                 else if (j == 2)
                 {
-                    itemUses = player_inv[i][j];
+//                    cout << "about to put value in itemUses.  Current value is " << itemUses << "\n";
+//                    itemUses = player_inv[i][j];
+//                    cout << "new value for itemUses is now " << itemUses <<"\n";
                     startNumber++;
                     cout << "    (" << startNumber << ") "  << findItemName(pos1, pos2) << "\n";
                 }
@@ -145,36 +142,46 @@ int equipMenu()
         int itemNumber = userInput - 1;
         pos1 = player_inv[itemNumber][0];
         pos2 = player_inv[itemNumber][1];
+        cout << "defining itemUses variable.  current value is " << itemUses << "\n";
         int itemUses = player_inv[itemNumber][2];
+        cout << "itemUses value is now " << itemUses << "\n";
         string item = findItemName(pos2, pos2);
-        cout << "itemUses is " << itemUses << "\n";
+        cout << "sending itemUses to equipItem().  current value is  " << itemUses << "\n";
         equipItem(pos1,pos2,itemUses);
         dropItem(itemNumber);
     }
+    cout << "ending equipMenu()";
 }
 
 void equipItem(int table, int itemNumber, int uses)
 {
+    cout << "starting equipItem().  The uses value I received is " << uses << "\n";
     equipWeapon(table, itemNumber, uses);
 }
 
 void equipWeapon(int table, int itemNumber, int uses)
 {
+    cout << "starting equipWeapon().  The uses value I received is " << uses << "\n";
     if (player_eweapon[0][0] != 69)
     {
         etable = player_eweapon[0][0];
         eitemNumber = player_eweapon[0][1];
+        cout << "creating euses variable.  The current value is" << euses << "\n";
         euses = player_eweapon[0][2];
+        cout << "The new value of euses is" << euses << "\n";
         addInventory(etable, eitemNumber, euses);
     }
     player_eweapon[0][0] = table;
     player_eweapon[0][1] = itemNumber;
+    cout << "makein player_eweapon uses equal to uses variable which is " << uses << "\n";
     player_eweapon[0][2] = uses;
+    cout << "player_eweapon[0][2] is now " << player_eweapon[0][2] << "\n";
+    cout << "ending equipWeapon()\n";
 }
 
 void dropItem(int itemNumber)
 {
-    for (int i = itemNumber; i < playerItemCount + 1; i++ )
+    for (int i = itemNumber; i < playerItemCount; i++ )
     {
         int movingI = i + 1;
         for (int j = 0; j < 2; j++)
@@ -187,21 +194,28 @@ void dropItem(int itemNumber)
 
 void addInventory(int table, int item, int itemUses)
 {
+    cout << "starting addInventory().  The itemUses I received is " << itemUses << "\n";
     if (playerItemCount < 25 )
     {
         player_inv[playerItemCount][0] = table;
         player_inv[playerItemCount][1] = item;
+        cout << "making player_inv uses equal to itemUses which is " << itemUses << "\n";
         player_inv[playerItemCount][2] = itemUses;
+        cout << "playerItemCount is " << playerItemCount << "\n";
+        cout << "player_inv[playerItemCount][2] is now " << player_inv[playerItemCount][2] << "\n";
         playerItemCount++;
+        cout << "playerItemCount is " << playerItemCount << "\n";
     }
     else
     {
         cout << "Inventory is full.";
     }
+    cout << "ending addInventory()";
 }
 
 void displayInventory()
 {
+    cout << "starting displayInventory()\n";
     for (int i = 0; i < playerItemCount; i++ )
     {
         string tableName;
@@ -220,12 +234,14 @@ void displayInventory()
                 itemUses = player_inv[i][j];
                 cout << "         " << "Item Location:  player_inv[" << pos1 << "][" << pos2 << "]\n";
                 cout << "         " << "Uses Location:  player_inv[" << i << "][" << j <<"]\n";
+                cout << "number of uses of following item coming from itemUses variable which is currently " << itemUses << "\n";
                 cout << "         " << findItemName(pos1, pos2) << " Uses " << itemUses << "\n";
                 cout << "//////////\n";
             }
         }
     }
     cout << "\n";
+    cout << "ending displayInventory()";
 }
 
 string findItemName(int table, int itemNumber)
